150a65d73a7320ba128a05581f79528c7e2fd253
Sanjukta1911 <116024814+Sanjukta1911@users.noreply.github.com>
2024-01-18 15:40:40 +0530

    initial commit

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,17 @@
+function Link({ uri, text }) {
+  return <a href={uri} target="_blank" rel="noreferrer">{text}</a>;
+}
+
+function Footer() {
+  return (
+    <footer>
+      <h5>CC</h5>
+
+    </footer >
+  );
+}
+
+export default Footer;
+
+
+
